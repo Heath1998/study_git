72f0ccebc0138c8576b28c5e9f30f4c8a0df7a4e
“zehongfan <fanzehongemail@163.com>
2021-01-11 14:13:54 +0800

    git test

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ console.log('hello-init');
 
 console.log("this is fan commit");
 
-console.log('this is mao commit');
+console.log('this is mao co12323123mmit');
+console.log('this is mao co12323123mmit');
+console.log('this is mao co12323123mmit');
+console.log('this is mao co12323123mmit');
+console.log('this is mao co12323123mmit');
+console.log('this is mao co12323123mmit');
+console.log('this is mao co12323123mmit');
 
